5670212ff733ec539254e7e68ca814df64eaecf5
Ladislav Mate <ladislav.mate@gmail.com>
2016-10-05 15:49:23 +0200

    Add E.1.1[78]

---
+++ C01/E.1.17.Lines.longer.then.80c.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define MAX_LINELEN 1000
+
+/* 
+ * Write a program to print all input lines that are longer than 80 characters.
+ */
+
+
+int main(int argc, char** argv)
+{
+	char c, text[MAX_LINELEN];	
+	int i = 0;
+
+	while ((c = getchar()) != EOF  ) {
+		if (c != '\n') {
+			text[i] = c;
+			++i;
+		}
+		if (c == '\n' && i >= 5) {
+			++i;
+			text[i] = '\0';
+			printf("%s\n",text);
+			i = 0;
+		}
+	}
+}
+
+
+
---
+++ C01/E.1.18.Remove.triling.blanks.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/*
+ * Write a program to remove trailing blanks and tabs from each line of input, and to delete entirely blank lines.
+ */
+
+#define MAX_LINE 10000
+
+int mygetline(char line[], int line_num)
+{
+	char c;
+	int i = 0;
+
+	while ((c = getchar()) != EOF)  {
+		line[i] = c;
+		i++;
+	}
+
+	return i;
+}
+
+int main(int argc, char** argv)
+{
+	char text[MAX_LINE];
+	int line = 0;
+	int line_len = 0;
+
+	line_len = mygetline(text,line);
+
+	printf("\n -> \t%s<-|",text);
+
+	for (int i = line_len; i >= 0; i--)
+		if (text[i] == '\n' && (text[i-1] == '\t' || text[i=1] == ' ') )
+			for (int j = i ; j < line_len; j++)
+				text[i-1] = text[i];
+
+	return 0;
+}
